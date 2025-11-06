2290e547321f57663c0d3ec5d71c79a123a8bf9f
crazypeace <txican@gmail.com>
2025-11-06 22:23:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,6 +30,9 @@ echo -e "有问题加群 ${cyan}https://t.me/+q5WPfGjtwukyZjhl${none}"
 echo -e "本脚本支持带参数执行, 省略交互过程, 详见GitHub."
 echo "----------------------------------------------------------------"
 
+# 确保有 curl 和 wget
+apt install -y curl wget
+
 # 本机 IP
 InFaces=($(ls /sys/class/net/ | grep -E '^(eth|ens|eno|esp|enp|venet|vif)'))
 
@@ -108,7 +111,7 @@ pause
 
 # 准备工作
 apt update
-apt install -y curl sudo jq qrencode net-tools lsof
+apt install -y sudo jq qrencode net-tools lsof
 
 # Xray官方脚本 安装最新版本
 echo
